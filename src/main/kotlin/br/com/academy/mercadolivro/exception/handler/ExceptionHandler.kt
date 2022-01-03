84a4226db89a6babcfbb029965d898066da53457
Nesomar Ramalho Freire <nfreire@latam.stefanini.com>
2022-01-03 12:00:58 -0300

    tratamento do 403 no ExceptionHandler

--- src/main/kotlin/br/com/academy/mercadolivro/exception/handler/ExceptionHandler.kt
+++ src/main/kotlin/br/com/academy/mercadolivro/exception/handler/ExceptionHandler.kt
@@ -7,6 +7,7 @@ import br.com.academy.mercadolivro.exception.model.ErrorResponse
 import br.com.academy.mercadolivro.exception.model.FieldErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -27,6 +28,21 @@ class ExceptionHandler {
     fun badRequestExceptionHandler(exception: BadRequestException, request: WebRequest): ResponseEntity<ErrorResponse> {
         return ResponseEntity.badRequest()
             .body(ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.message, exception.errorCode))
+    }
+
+    @ExceptionHandler(AccessDeniedException::class)
+    fun accessDeniedExceptionHandler(
+        exception: AccessDeniedException,
+        request: WebRequest
+    ): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            HttpStatus.FORBIDDEN.value(),
+            CommonErrorCode.ML000.message,
+            CommonErrorCode.ML000.code,
+            null
+        )
+
+        return ResponseEntity(error, HttpStatus.FORBIDDEN)
     }
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
